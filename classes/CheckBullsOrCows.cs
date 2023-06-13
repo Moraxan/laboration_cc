928f2db1d15fe57e6487d28a78b7445ce5318026
Carl-Johan Svensson <calletrumma@gmail.com>
2023-06-13 14:07:18 +0200

    Creating classes

---
+++ classes/CheckBullsOrCows.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MooGame
+{
+    public class CheckBullsOrCows
+    {
+      public string checkBullsOrCows(string goal, string guess)
+		{
+			int cows = 0, bulls = 0;
+			guess += "    ";     // if player entered less than 4 chars
+			for (int i = 0; i < 4; i++)
+			{
+				for (int j = 0; j < 4; j++)
+				{
+					if (goal[i] == guess[j])
+					{
+						if (i == j)
+						{
+							bulls++;
+						}
+						else
+						{
+							cows++;
+						}
+					}
+				}
+			}
+			return "BBBB".Substring(0, bulls) + "," + "CCCC".Substring(0, cows);
+		}
+	}  
+}
