08a6253aa7659844ecbb04af33ae687f638b196a
Carl-Johan Svensson <calletrumma@gmail.com>
2023-06-13 14:16:11 +0200

    Logic is working again

--- Program.cs
+++ Program.cs
@@ -32,23 +32,23 @@ namespace MooGame
 				Console.WriteLine("For practice, number is: " + goal + "\n");
 				string? guess = Console.ReadLine();
 				CheckBullsOrCows checkBullsOrCows = new CheckBullsOrCows();
-				int nGuess = 1;
+				int numberOfGuesses = 1;
 				string bbcc = checkBullsOrCows.checkBullsOrCows(goal, guess);
 				Console.WriteLine(bbcc + "\n");
 				while (bbcc != "BBBB,")
 				{
-					nGuess++;
+					numberOfGuesses++;
 					guess = Console.ReadLine();
 					Console.WriteLine(guess + "\n");
 					bbcc = checkBullsOrCows.checkBullsOrCows(goal, guess);
 					Console.WriteLine(bbcc + "\n");
 				}
 				StreamWriter output = new StreamWriter("result.txt", append: true);
-				output.WriteLine(name + "#&#" + nGuess);
+				output.WriteLine(name + "#&#" + numberOfGuesses);
 				output.Close();
 				TopList topList = new TopList();
 				topList.Show();
-				Console.WriteLine("Correct, it took " + nGuess + " guesses\nContinue?");
+				Console.WriteLine("Correct, it took " + numberOfGuesses + " guesses\nContinue?");
 				string? answer = Console.ReadLine();
 				if (answer != null && answer != "" && answer.Substring(0, 1) == "n")
 				{
