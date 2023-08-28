a54078687d19f6a5acef5a3dd0c4dc86ce4f24f7
Moraxan <83705522+Moraxan@users.noreply.github.com>
2023-08-28 11:49:41 +0200

    Minor changes

--- classes/CheckResult.cs
+++ classes/CheckResult.cs
@@ -1,26 +1,24 @@
 public class Result
 {
-	public string CheckResult (string goal, string guess)
-	{
-		int cows = 0, bulls = 0;
-		guess += "    ";     // if player entered less than 4 chars
-		for (int i = 0; i < 4; i++)
-		{
-			for (int j = 0; j < 4; j++)
-			{
-				if (goal[i] == guess[j])
-				{
-					if (i == j)
-					{
-						bulls++;
-					}
-					else
-					{
-						cows++;
-					}
-				}
-			}
-		}
-		return "BBBB".Substring(0, bulls) + "," + "CCCC".Substring(0, cows);
-	}
+    public string CheckResult(string goal, string guess)
+    {
+        int cows = 0, bulls = 0;
+
+        guess += new string(' ', Math.Max(0, 4 - guess.Length)); 	//  Ensures length is 4. There is a risk this doesn't handle inputs longer tha 4 chars well, 
+																	// but should atleast make it cut it after 4 chars.
+
+        for (int i = 0; i < 4; i++)
+        {
+            if (goal[i] == guess[i])
+            {
+                bulls++;
+            }
+            else if (goal.Contains(guess[i]))
+            {
+                cows++;
+            }
+        }
+
+        return new string('B', bulls) + "," + new string('C', cows);
+    }
 }
