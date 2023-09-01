0ba56fbbaed03c45119374309ec07547206d99d7
Moraxan <83705522+Moraxan@users.noreply.github.com>
2023-09-01 12:32:18 +0200

    Namespace change

--- classes/ConsoleUI.cs
+++ classes/ConsoleUI.cs
@@ -1,33 +1,36 @@
-    public class ConsoleUI : IUserInterface
-    {
-        public string? GetUserInput(string prompt)
-        {
-            while (true) // Keep looping until valid input is provided
-            {
-                Console.WriteLine(prompt);
-                try
-                {
-                    string? input = Console.ReadLine();
+namespace laboration_cc.classes
+{
+	public class ConsoleUI : IUserInterface
+	{
+		public string? GetUserInput(string prompt)
+		{
+			while (true) // Keep looping until valid input is provided
+			{
+				Console.WriteLine(prompt);
+				try
+				{
+					string? input = Console.ReadLine();
 
-                    if (!string.IsNullOrEmpty(input)) // Check for non-null and non-empty input
-                    {
-                        return input;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Input cannot be empty. Please try again.");
-                    }
-                }
-                catch (NullReferenceException)
-                {
-                    // Handle the case where the user input is null (Enter pressed)
-                    Console.WriteLine("Invalid input. Please try again.");
-                }
-            }
-        }
+					if (!string.IsNullOrEmpty(input)) // Check for non-null and non-empty input
+					{
+						return input;
+					}
+					else
+					{
+						Console.WriteLine("Input cannot be empty. Please try again.");
+					}
+				}
+				catch (NullReferenceException)
+				{
+					// Handle the case where the user input is null (Enter pressed)
+					Console.WriteLine("Invalid input. Please try again.");
+				}
+			}
+		}
 
-        public void DisplayMessage(string message)
-        {
-            Console.WriteLine(message);
-        }
-    }
+		public void DisplayMessage(string message)
+		{
+			Console.WriteLine(message);
+		}
+	}
+}
